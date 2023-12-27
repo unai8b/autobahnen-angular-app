676d80c6385972caa350624c7b86a61717c39588
Unai Benajes <unai8b@gmail.com>
2023-12-27 21:14:09 +0100

    feat: improve home page
    Redesign and improve home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -6,11 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  public pageTitle: string;
   public linkedin: string; 
   public gmail: string;
 
   constructor() {
+    this.pageTitle = 'Welcome!';
     this.linkedin = 'Unai Benajes Esbrí';
     this.gmail = 'unai8b@gmail.com';
+  }
+
+  openLinkedIn(): void {
+    window.open('https://www.linkedin.com/in/unai-benajes-esbrí/', '_blank');
+  }
+
+  openGmail(): void {
+    window.open('mailto:unai8b@gmail.com', '_blank');
   }
 }
